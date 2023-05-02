a1565d48993c731a8fe85750941e317be3be7087
Max <gang121191@gmail.com>
2023-05-02 22:08:30 +0300

    ?

---
+++ Lab4/Program.cs
@@ -0,0 +1,17 @@
+﻿string text = File.ReadAllText($@"{Environment.CurrentDirectory}\some.txt");
+
+char[] symbols = text.ToCharArray();
+
+//var symbols = text.ToList();
+
+int numberOfSymbols = 0;
+
+foreach(char symbol in symbols)
+{
+    if (Char.IsPunctuation(symbol))
+    {
+        numberOfSymbols++;
+    }
+}
+
+Console.WriteLine(numberOfSymbols);
